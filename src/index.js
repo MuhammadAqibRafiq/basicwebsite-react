2a502f1125ca8d79ecd63bf00fc1e3cb18253a40
Muhammadaqibrafiq <aqibrafiq001@outlook.com>
2021-04-21 14:13:30 +0500

    first commit

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 
 Reactdom.render(
   <>
-    <h1 className="heading_style">Top 5 netflix movies 2021</h1>,
+    <h1 className="heading_style">Top 5 netflix movies </h1>,
     {Sdata.map((val,index) => {
       return (
         <Card
